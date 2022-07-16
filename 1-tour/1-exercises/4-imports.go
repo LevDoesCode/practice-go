705f9b8bddc6793d036cae49d283d6064bcbee18
Lev <levdoescode@gmail.com>
2022-07-15 20:48:47 -0500

    Add Initial commit

---
+++ 1-tour/1-exercises/4-imports.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// This is also possible, but the style above is recommended
+// import "fmt"
+// import "math"
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
