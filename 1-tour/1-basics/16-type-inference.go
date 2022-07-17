1430cb9da80fb11ebdbca3262f7e103cd94beb9c
Lev <levdoescode@gmail.com>
2022-07-17 17:43:49 -0500

    Add Initial commit

---
+++ 1-tour/1-basics/16-type-inference.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42
+	fmt.Printf("v is of type %T\n", v)
+}
+
+// var i int
+// j := i // j is an int
+// i := 42           // int
+// f := 3.142        // float64
+// g := 0.867 + 0.5i // complex128
