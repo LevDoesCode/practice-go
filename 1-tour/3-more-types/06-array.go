27eea798ab884c90af0e956b0e913dc60c956c92
Lev <levdoescode@gmail.com>
2022-07-19 09:46:44 -0500

    Add Initial commit

---
+++ 1-tour/3-more-types/06-array.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	booleans := []bool{true, false, true}
+	fmt.Println(primes)
+	fmt.Println(booleans)
+	fmt.Println(len(primes))
+	fmt.Println(len(booleans))
+}
