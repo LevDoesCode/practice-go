3c049b5ad01bed9b587f19d5643db280f97cf495
Lev <levdoescode@gmail.com>
2022-07-20 12:58:16 -0500

    Add Initial commit

---
+++ 1-tour/3-more-types/13-making-slices.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := make([]int, 5)
+	printSlice("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+
+	c := b[:2]
+	printSlice("c", c)
+
+	d := c[2:5]
+	printSlice("d", d)
+
+	// this will throw an error because the capacity
+	// of the underlying array is just 5
+	// e := a[0:10]
+	// printSlice("e", e)
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
