f295e7542faa82de2337e657e8bdb143bd3489ba
Lev <levdoescode@gmail.com>
2022-07-20 13:19:33 -0500

    Update array auto count using [...]

--- 1-tour/3-more-types/06-array.go
+++ 1-tour/3-more-types/06-array.go
@@ -10,9 +10,18 @@ func main() {
 	fmt.Println(a)
 
 	primes := [6]int{2, 3, 5, 7, 11, 13}
-	booleans := []bool{true, false, true}
+	booleans := []bool{true, false, true} // This is actually a slice :)
+
+	strings := [...]string{"Penn", "Teller"}
+
 	fmt.Println(primes)
 	fmt.Println(booleans)
+	fmt.Println(strings)
 	fmt.Println(len(primes))
 	fmt.Println(len(booleans))
+	fmt.Println(len(strings))
+
+	fmt.Printf("Type of primes: %T\n", primes)
+	fmt.Printf("Type of booleans: %T\n", booleans)
+	fmt.Printf("Type of strings: %T\n", strings)
 }
