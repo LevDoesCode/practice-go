b783fec0a62bf224052b87f9db2bce49919fceca
Lev <levdoescode@gmail.com>
2022-07-26 18:43:57 -0500

    Add Initial commit

---
+++ 1-tour/4-methods-interfaces/17-stringer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+}
+
+func main() {
+	a := Person{"Arthur Dent", 42}
+	z := Person{"Zaphod Beeblebrox", 9001}
+	fmt.Println(a, z)
+}
+
+// Println looks for a method bound to the type of the variable
+// called String and that returns a string, and calls it if present
