6009b9c975f28a93c598b304d81135543ba97976
Lev <levdoescode@gmail.com>
2022-07-31 16:09:20 -0500

    git commit

---
+++ 1-tour/5-generics/02-list.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+// List represents a singly-linked list that holds
+// values of any type.
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) GetAll() []T {
+	var elements []T
+	for e := lst.head; e != nil; e = e.next {
+		elements = append(elements, e.val)
+	}
+	return elements
+}
+
+func main() {
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(24)
+	fmt.Println("list:", lst.GetAll())
+}
