4995f20d681341f33b6493d75ff380e6108958fa
Lev <levdoescode@gmail.com>
2022-07-31 16:29:15 -0500

    Update added another solution

--- 1-tour/4-methods-interfaces/25-exercise-images.go
+++ 1-tour/4-methods-interfaces/25-exercise-images.go
@@ -58,6 +58,36 @@ func main() {
 	pic.ShowImage(newImg)
 }
 
+/////////////////////////////
+// Another solution
+
+// package main
+
+// import (
+//     "golang.org/x/tour/pic"
+//     "image/color"
+//     "image"
+// )
+
+// type Image struct{
+//     Rect image.Rectangle
+// }
+
+// func (image Image) ColorModel() color.Model {
+//         return color.RGBAModel
+// }
+// func (i Image) Bounds() image.Rectangle {
+//     return i.Rect
+// }
+// func (image Image) At(x, y int) color.Color {
+//     return color.RGBA{uint8(x*y), uint8(y+x), 255, 255}
+// }
+
+// func main() {
+//     m := Image{image.Rect(0,0,300,300)}
+//     pic.ShowImage(m)
+// }
+//////////////////////////////////////
 // Another solution
 // package main
 
