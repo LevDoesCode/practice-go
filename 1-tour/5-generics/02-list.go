09020c168e744462c77adbda03e77fd5b3604426
Lev <levdoescode@gmail.com>
2022-07-31 16:28:26 -0500

    Add Initial commit

--- 1-tour/5-generics/02-list.go
+++ 1-tour/5-generics/02-list.go
@@ -32,10 +32,21 @@ func (lst *List[T]) GetAll() []T {
 	return elements
 }
 
+// Using a stringer
+func (lst *List[T]) String() string {
+	var elements []T
+	for e := lst.head; e != nil; e = e.next {
+		elements = append(elements, e.val)
+	}
+	return fmt.Sprint(elements)
+}
+
 func main() {
 	lst := List[int]{}
 	lst.Push(10)
 	lst.Push(13)
 	lst.Push(24)
-	fmt.Println("list:", lst.GetAll())
+	fmt.Println("List:", lst.GetAll())
+	fmt.Printf("%T\n", lst)
+	fmt.Println("List:", &lst) // We send a pointer to lst
 }
