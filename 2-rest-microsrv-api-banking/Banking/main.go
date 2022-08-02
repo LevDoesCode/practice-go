f386ea202b62c50c777f670d71424f953d83d012
Lev <levdoescode@gmail.com>
2022-08-02 13:20:26 -0500

    Update added support for xml in the request header

--- 2-rest-microsrv-api-banking/Banking/main.go
+++ 2-rest-microsrv-api-banking/Banking/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,9 +11,9 @@ import (
 // By the default, the json response will use the struct definition for the keys
 // If we want to change them we can add the tags in backticks ``
 type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	ZipCode string `json:"zip_code"`
+	Name    string `json:"full_name" xml:"full_name"`
+	City    string `json:"city" xml:"city"`
+	ZipCode string `json:"zip_code" xml:"zip_code"`
 }
 
 func main() {
@@ -40,7 +41,13 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		{Name: "Lev", City: "Lima", ZipCode: "15103"},
 		{Name: "Roger", City: "Springfield", ZipCode: "10001"},
 	}
-	// Set the correct response header for the response writer from plain text to json
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(customers)
+	// Set the correct response header for the response writer from plain text to json or xml
+	//fmt.Printf("%s\n", r.Header.Get("Content-Type"))
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
 }
