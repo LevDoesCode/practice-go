f7a36644a65f2fd5686fe2ae36092f64e092eff4
Lev <levdoescode@gmail.com>
2022-08-01 22:17:11 -0500

    Updated

---
+++ 2-rest-microsrv-api-banking/Banking/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// We register the pattern and the handler function
+	// The default multiplexer is used by HandleFunc
+	//http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Fprintf(w, "Hello, world!")
+	//})
+	//  define routes
+	http.HandleFunc("/greet", greet)
+	// starting server
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	// ListenAndServe returns an error we can check if there's an error starting the server
+	// http.ListenAndServe("localhost:8080", nil)
+	// we pass a nil handler because we're not creating our own
+
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
+}
