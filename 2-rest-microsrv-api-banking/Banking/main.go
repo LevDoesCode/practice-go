f7bbe4359da8e600ef3edbb34d26551991e6ccbf
Lev <levdoescode@gmail.com>
2022-08-02 00:18:05 -0500

    Update added json enconder & getAllCustomers route

--- 2-rest-microsrv-api-banking/Banking/main.go
+++ 2-rest-microsrv-api-banking/Banking/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+// By the default, the json response will use the struct definition for the keys
+// If we want to change them we can add the tags in backticks ``
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	ZipCode string `json:"zip_code"`
+}
 
 func main() {
 	// We register the pattern and the handler function
 	// The default multiplexer is used by HandleFunc
 	//http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
 	//	fmt.Fprintf(w, "Hello, world!")
-	//})
+	//}) // replace by a using a function name as parameter
 	//  define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 	// starting server
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 	// ListenAndServe returns an error we can check if there's an error starting the server
@@ -23,4 +33,14 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "Lev", City: "Lima", ZipCode: "15103"},
+		{Name: "Roger", City: "Springfield", ZipCode: "10001"},
+	}
+	// Set the correct response header for the response writer from plain text to json
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
