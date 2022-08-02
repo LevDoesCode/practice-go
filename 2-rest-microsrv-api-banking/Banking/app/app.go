9b03b43c5c0978b0115fed822b88bcb8ace4a7ad
Lev <levdoescode@gmail.com>
2022-08-02 13:48:10 -0500

    Update starter code moved here, added custom mux

---
+++ 2-rest-microsrv-api-banking/Banking/app/app.go
@@ -0,0 +1,25 @@
+package app
+
+import (
+	"log"
+	"net/http"
+)
+
+func StartServer() {
+	// We register the pattern and the handler function
+	// The default multiplexer is used by HandleFunc
+	//http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Fprintf(w, "Hello, world!")
+	//}) // replace by a using a function name as parameter
+	// we create a new multiplexer that can be customized instead of the default one
+	mux := http.NewServeMux()
+
+	//  define routes
+	mux.HandleFunc("/greet", greet)
+	mux.HandleFunc("/customers", getAllCustomers)
+	// starting server
+	log.Fatal(mux.ListenAndServe("localhost:8080", nil))
+	// ListenAndServe returns an error we can check if there's an error starting the server
+	// http.ListenAndServe("localhost:8080", nil)
+	// we pass a nil handler because we're not creating our own
+}
