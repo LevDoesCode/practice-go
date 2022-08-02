40423eeb196bc46d16d4ebe96871623423fb695e
Lev <levdoescode@gmail.com>
2022-08-02 13:29:16 -0500

    Add Initial commit functionality from main

---
+++ 2-rest-microsrv-api-banking/Banking/handlers.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"net/http"
+)
+
+// By the default, the json response will use the struct definition for the keys
+// If we want to change them we can add the tags in backticks ``
+type Customer struct {
+	Name    string `json:"full_name" xml:"full_name"`
+	City    string `json:"city" xml:"city"`
+	ZipCode string `json:"zip_code" xml:"zip_code"`
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "Lev", City: "Lima", ZipCode: "15103"},
+		{Name: "Roger", City: "Springfield", ZipCode: "10001"},
+	}
+	// Set the correct response header for the response writer from plain text to json or xml
+	//fmt.Printf("%s\n", r.Header.Get("Content-Type"))
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
+}
