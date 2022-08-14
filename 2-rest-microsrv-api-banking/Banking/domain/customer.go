862bddf98063b20c9c980f11282d4390fcc87dc8
Lev <levdoescode@gmail.com>
2022-08-13 23:45:46 -0500

    Update implemented DTO for struct and helper func

--- 2-rest-microsrv-api-banking/Banking/domain/customer.go
+++ 2-rest-microsrv-api-banking/Banking/domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "Banking/errs"
+import (
+	"Banking/dto"
+	"Banking/errs"
+)
 
 // Customer is the domain object
 type Customer struct {
@@ -11,6 +14,37 @@ type Customer struct {
 	City    string `db:"city"`
 	ZipCode string `db:"zipcode"`
 	Status  string `db:"status"`
+}
+
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+func (c Customer) ToDTO() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		Id:      c.Id,
+		Name:    c.Name,
+		DOB:     c.DOB,
+		City:    c.City,
+		ZipCode: c.ZipCode,
+		Status:  c.statusAsText(),
+	}
+}
+
+func AllToDTO(customers []Customer, status string) []dto.CustomerResponse {
+	var allDTO []dto.CustomerResponse
+	for _, customer := range customers {
+		if status == "" {
+			allDTO = append(allDTO, customer.ToDTO())
+		} else if customer.Status == status {
+			allDTO = append(allDTO, customer.ToDTO())
+		}
+	}
+	return allDTO
 }
 
 // CustomerRepository is the Secondary port that will connect to the server side
