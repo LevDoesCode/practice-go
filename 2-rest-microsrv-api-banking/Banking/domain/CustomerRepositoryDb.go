14db58b6583143a234e9d5027455c38b460f1482
Lev <levdoescode@gmail.com>
2022-08-13 22:11:10 -0500

    Update functions FindAll and ByID with sqlx

--- 2-rest-microsrv-api-banking/Banking/domain/CustomerRepositoryDb.go
+++ 2-rest-microsrv-api-banking/Banking/domain/CustomerRepositoryDb.go
@@ -4,24 +4,29 @@ import (
 	"Banking/errs"
 	"Banking/logger"
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
 type CustomerRepositoryDb struct {
-	client *sql.DB
+	client *sqlx.DB
 }
 
 func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError) {
-	var rows *sql.Rows
+	// var rows *sql.Rows // We don't use a sql.Rows object and iterate through it anymore
 	var err error
+	customers := make([]Customer, 0)
 
 	if status == "" {
 		findAllSql := "select customer_id, name, dob, city, zipcode, status from customers"
-		rows, err = d.client.Query(findAllSql)
+		err = d.client.Select(&customers, findAllSql) // sqlx queries and marshals at the same time
+		//rows, err = d.client.Query(findAllSql)
 	} else {
 		findAllSql := "select customer_id, name, dob, city, zipcode, status from customers where status = ?"
-		rows, err = d.client.Query(findAllSql, status)
+		err = d.client.Select(&customers, findAllSql, status) // sqlx queries and marshals at the same time
+		//rows, err = d.client.Query(findAllSql, status)
 	}
 
 	if err != nil {
@@ -29,24 +34,32 @@ func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError
 		return nil, errs.NewUnexpectedError("Unexpected database error")
 	}
 
-	customers := make([]Customer, 0)
-	for rows.Next() {
-		var c Customer
-		err := rows.Scan(&c.Id, &c.Name, &c.DOB, &c.City, &c.ZipCode, &c.Status)
-		if err != nil {
-			logger.Error("Error while scanning customers: " + err.Error())
-			return nil, errs.NewUnexpectedError("Unexpected database error")
-		}
-		customers = append(customers, c)
-	}
+	// We don't need to use the sqlx StructScan anymore since the marshalling has been done by sqlx.Select
+	//err = sqlx.StructScan(rows, &customers)
+	//if err != nil {
+	//	logger.Error("Error while scanning customers: " + err.Error())
+	//	return nil, errs.NewUnexpectedError("Unexpected database error")
+	//}
+
+	// for rows.Next() {
+	// 	var c Customer
+	// 	err := rows.Scan(&c.Id, &c.Name, &c.DOB, &c.City, &c.ZipCode, &c.Status)
+	// 	if err != nil {
+	// 		logger.Error("Error while scanning customers: " + err.Error())
+	// 		return nil, errs.NewUnexpectedError("Unexpected database error")
+	// 	}
+	// 	customers = append(customers, c)
+	// }
 	return customers, nil
 }
 
+// Rows -> sqlx.Select QueryRow -> sqlx.Get
 func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
 	customerSql := "select customer_id, name, dob, city, zipcode, status from customers where customer_id = ?"
-	row := d.client.QueryRow(customerSql, id)
+	//row := d.client.QueryRow(customerSql, id) // This still works as an sqlx.DB object, but we can use Get
 	var c Customer
-	err := row.Scan(&c.Id, &c.Name, &c.DOB, &c.City, &c.ZipCode, &c.Status)
+	err := d.client.Get(&c, customerSql, id)
+	//err := row.Scan(&c.Id, &c.Name, &c.DOB, &c.City, &c.ZipCode, &c.Status)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, errs.NewNotFoundError("Customer not found")
@@ -60,7 +73,8 @@ func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
 
 func NewCustomerRepositoryDb() CustomerRepositoryDb {
 	// mysql driver code from https://github.com/go-sql-driver/mysql
-	client, err := sql.Open("mysql", "root:pass123@tcp(localhost:3306)/banking")
+	//client, err := sql.Open("mysql", "root:pass123@tcp(localhost:3306)/banking")
+	client, err := sqlx.Open("mysql", "root:pass123@tcp(localhost:3306)/banking")
 	if err != nil {
 		panic(err)
 	}
